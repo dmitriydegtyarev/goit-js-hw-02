f054d4c14a87f60a7a534bd55216adb10c3780b7
Dmitriy Dihtiarov <dihtiarovd@gmail.com>
2020-12-28 22:38:36 +0200

    finish task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,22 +1,24 @@
-const formatString = function(string) {
-  // твой код
+const formatString = function (string) {
+  
+  const stringLength = string.length;
+  
+  // if (stringLength <= 40) {
+  //   console.log(string);
+  // } else {
+  //   console.log(string.slice(0, 40) + '...');
+  // }
+
+  return stringLength <= 40 ? console.log(string) : console.log(string.slice(0, 40) + '...');
 };
 
-/*
- * Вызовы функции для проверки работоспособности твоей реализации.
- */
-console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
+formatString('Curabitur ligula sapien, tincidunt non.');
 // вернется оригинальная строка
 
-console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
+formatString('Vestibulum facilisis, purus nec pulvinar iaculis.');
 // вернется форматированная строка
 
-console.log(formatString('Curabitur ligula sapien.'));
+formatString('Curabitur ligula sapien.');
 // вернется оригинальная строка
 
-console.log(
-  formatString(
-    'Nunc sed turpis. Curabitur a felis in nunc fringilla tristique.',
-  ),
-);
+formatString('Nunc sed turpis. Curabitur a felis in nunc fringilla tristique.');
 // вернется форматированная строка
