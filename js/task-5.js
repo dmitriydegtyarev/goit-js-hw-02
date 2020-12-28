9e54697db6be4f3038f138dda8a9f1ea1d09d8e3
Dmitriy Dihtiarov <dihtiarovd@gmail.com>
2020-12-29 00:24:14 +0200

    finish task 5

---
+++ js/task-5.js
@@ -0,0 +1,14 @@
+const checkForSpam = function(message) {
+  let spamAndSale = message.toLowerCase();
+
+  return spamAndSale.includes('spam') || spamAndSale.includes('sale') ? true : false;  
+};
+
+console.log(checkForSpam('Latest technology news')); // false
+
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+
+console.log(checkForSpam('Get best sale offers now!')); // true
+
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+
